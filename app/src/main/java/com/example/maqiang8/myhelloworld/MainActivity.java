2b6cd683d86faf67a9016d2f2a29687c4acadd01
maqiang <maqiang8@jd.com>
2017-08-08 20:37:53 +0800

    add log

--- app/src/main/java/com/example/maqiang8/myhelloworld/MainActivity.java
+++ app/src/main/java/com/example/maqiang8/myhelloworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.maqiang8.myhelloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +15,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public static void print(){
-
+        Log.e("dk","kdk");
     }
 }
