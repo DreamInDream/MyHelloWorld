946696bef17e2cbbf441c0c48b511813a3bfc130
maqiang <maqiang8@jd.com>
2017-08-18 15:31:05 +0800

    add log

--- app/src/main/java/com/example/maqiang8/myhelloworld/MainActivity.java
+++ app/src/main/java/com/example/maqiang8/myhelloworld/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"kk",Toast.LENGTH_SHORT).show();
-
+        Toast.makeText(this,"kk",Toast.LENGTH_SHORT).show();
     }
     public static void print(){
         Log.e("dk","kdk");
